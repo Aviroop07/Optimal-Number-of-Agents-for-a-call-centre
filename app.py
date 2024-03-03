5d72f1396cfea1bd388476ae79ebce10f97986b5
Aviroop Mitra <94693971+Aviroop07@users.noreply.github.com>
2024-03-04 01:46:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 import streamlit as st
 import pandas as pd
-!pip install prophet
-from prophet import Prophet
 import numpy as np
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 from pandas.tseries.holiday import MO
 SHIFT = 12
+import subprocess
+def install_package(package_name):
+    try:
+        # Check if the package is already installed
+        subprocess.check_call(["pip", "show", package_name])
+    except subprocess.CalledProcessError:
+        # If the package is not installed, install i
+        subprocess.check_call(["pip", "install", package_name])
+
+# Call the function with the package name you want to install
+install_package("prophet")
+from prophet import Prophet
 
 # header 
 st.header("Call Center Prediction",divider="rainbow")
