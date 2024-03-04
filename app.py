2dfe7028ca9a7fe10199f92120c6545c929bf4c4
Aviroop Mitra <aviroopmitra5@gmail.com>
2024-03-04 10:04:20 +0530

    changes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import subprocess
-subprocess.check_call(["pip", "install", "-r", "requirements.txt"])
+subprocess.check_call(["pip", "install", "-r", "requirements.txt","--verbose"])
 import streamlit as st
 import pandas as pd
 import numpy as np
